f0ad864b2a093a85ad7d9bdb592b673db4dfb66c
rudus1012 <rudus1012@naver.com>
2022-02-09 16:31:41 +0900

    feat: 🎸 updateTodo

--- src/main/kotlin/com/kotlin/todolist/service/TodoService.kt
+++ src/main/kotlin/com/kotlin/todolist/service/TodoService.kt
@@ -2,6 +2,7 @@ package com.kotlin.todolist.service
 
 import com.kotlin.todolist.repository.Todo
 import com.kotlin.todolist.repository.TodoRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,4 +12,10 @@ class TodoService(
     fun getTodos() = todoRepository.findAll()
 
     fun insertTodo(todoName: String) = todoRepository.save(Todo(todoName = todoName))
+
+    fun updateTodo(todoId: Long): Todo {
+        val todo = todoRepository.findByIdOrNull(todoId) ?: throw Exception()
+        todo.completed = !todo.completed
+        return todoRepository.save(todo)
+    }
 }
